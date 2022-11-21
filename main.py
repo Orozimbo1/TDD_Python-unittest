0f93ee4344f7c283ed49f50a8779aa74caa0d788
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2022-11-21 20:05:01 -0300

    Assertions

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from calculadora import soma
 
-print(soma(10, 20))
-print(soma(-10, 20))
+# print(soma(10, 20))
+# print(soma(-10, 20))
+
+try:
+    print(soma('10', 20))
+except AssertionError as e:
+    print(f'Conta invalida: {e}')
+
+print('oi', soma(25, 25))
